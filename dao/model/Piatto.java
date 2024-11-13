7b338520dae6906c67cab14d9c7acde6117eebd0
SiMo079 <simonegaraglio@libero.it>
2024-11-13 15:06:38 +0100

    /dao

---
+++ dao/model/Piatto.java
@@ -0,0 +1,18 @@
+package com.dipartimento.demowebapplications.model;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Piatto {
+
+    private String nome;
+    private String ingredienti;
+
+}
