0fc49d833a89af537db08b3835390f4726d36701
SiMo079 <simonegaraglio@libero.it>
2024-11-20 17:36:35 +0100

    Update Piatto.java

--- dao/model/Piatto.java
+++ dao/model/Piatto.java
@@ -1,18 +1,15 @@
-package com.dipartimento.demowebapplications.model;
-
-import lombok.*;
-
-import java.util.List;
-
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
+@Entity
 public class Piatto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
-    private String ingredienti;
+    private String descrizione;
+    private Double prezzo;
 
+    @ManyToMany(mappedBy = "piatti", cascade = CascadeType.ALL)
+    private Set<Ristorante> ristoranti = new HashSet<>();
+
+    // Costruttori, getter e setter
 }
